00693cb38c68aa00e7443f1b8f0dc30e6e2172be
Brainner Rodrigues <brainner.trabalho@gmail.com>
2026-05-01 21:14:41 -0300

    rodando no android s8

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,7 +14,20 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // --- INÍCIO DA CORREÇÃO PARA O NAMESPACE ---
+    afterEvaluate {
+        if (project.extensions.findByName("android") != null) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            if (android.namespace == null) {
+                // Usa o ID do grupo do projeto como namespace fallback
+                android.namespace = project.group.toString()
+            }
+        }
+    }
+    // --- FIM DA CORREÇÃO ---
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
